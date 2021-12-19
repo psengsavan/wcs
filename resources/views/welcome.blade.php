6b7301703aeb9fea4b1493ca79060523c1989e8a
sengsavanphoungmalay <p.sengsavan@it-students.fr>
2021-12-20 00:52:31 +0100

    lint

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <title>Document</title>
+    <title>Liste des Agronautes</title>
 </head>
 <body>
-<header>
-  <h1>
-    <img src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
-    Les Argonautes
-  </h1>
-</header>
-<div class="container"></div>
-    <!-- Header section -->
-  
+
+  <!-- Header section -->
+  <header>
+    <h1>
+      <img src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
+      Les Argonautes
+    </h1>
+  </header>
+
   <!-- Main section -->
   <main>
     
@@ -28,6 +28,7 @@
       <input id="name" name="name" type="text" placeholder="Charalampos" required/>
       <button type="submit">Envoyer</button>
     </form>
+    
     @if (session()->has("message"))
       <div class="message">{{ session()->get("message") }}</div>
     @endif
@@ -38,15 +39,11 @@
       @foreach ($agronautes as $agronaute)
         <div class="member-item">{{ $agronaute->name }}</div>
       @endforeach
-      {{-- <div class="member-item">Eleftheria</div>
-      <div class="member-item">Gennadios</div>
-      <div class="member-item">Lysimachos</div> --}}
     </section>
   </main>
   
   <footer>
     <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
   </footer>
-</div>    
 </body>
 </html>
